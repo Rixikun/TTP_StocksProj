213029eb331eac0df6c1aa127b20f34e197df521
Lin Xia <linyianxia@hotmail.com>
2020-02-24 01:38:11 -0500

    refined data on  post purchase

--- server/controllers/transaction_controller.js
+++ server/controllers/transaction_controller.js
@@ -14,12 +14,52 @@ exports.getAllTransactions = async (req, res, next) => {
 
 exports.getUserTransactions = async (req, res, next) => {
   try {
-    // const transactions = await Transaction.findByPk(req.params.userId, {
-    //   include: [{ model: Transaction }]
-    // });
-    const transactions = await Transaction.findByPk(req.body.userId);
+    const transactions = await Transaction.findAll({
+      where: { userId: req.body.userId },
+      attributes: ["id", "totalPrice", "quantity", "createdAt"]
+    });
+    const stocks = await Stock.findAll({
+      where: { userId: req.body.userId }
+    });
+    const combined = stocks.map(stock => {
+      for (let i = 0; i < transactions.length; i++) {
+        let item = transactions[i];
+        if (item.id === stock.transactionId) {
+          return {
+            transactionId: item.id,
+            stockId: stock.id,
+            quantity: item.quantity,
+            totalPrice: item.totalPrice,
+            price: stock.price,
+            date: item.createdAt
+          };
+        }
+      }
+    });
     // console.log(transactions);
-    res.json(transactions);
+    res.json(combined);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postPurchase = async (req, res, next) => {
+  try {
+    const transaction = await Transaction.create({
+      quantity: req.body.quantity,
+      userId: req.body.userId
+    });
+    const stock = await Stock.create({
+      ticker: req.body.ticker,
+      transactionId: transaction.id,
+      userId: transaction.userId,
+      price: 50,
+      open: 40
+    });
+    transaction.totalPrice = req.body.quantity * stock.price;
+    transaction.save();
+    // console.log(transaction);
+    res.json(transaction);
   } catch (err) {
     next(err);
   }
