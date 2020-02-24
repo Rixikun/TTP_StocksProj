4982214e7b65e4a530189759df8920ae71cfff81
Lin Xia <linyianxia@hotmail.com>
2020-02-24 00:27:39 -0500

    added checklist. can post transactions

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const { isAdminMiddleware, isUserMiddleware } = require("../security");
-const { Transaction } = require("../db/models");
+const { Transaction, Stock, User } = require("../db/models");
 const transaction_control = require("../controllers/transaction_controller");
 
 module.exports = router;
@@ -11,31 +11,52 @@ router.get("/", transaction_control.getAllTransactions);
 
 router.get("/userstocks", transaction_control.getUserTransactions);
 
-router.post("/create", async (req, res) => {
-  try {
-    const savedOrder = await Transaction.create(
-      req.body,
-      { w: 1 },
-      { returning: true }
-    );
-    req.body.stocks.forEach(async item => {
-      const stock = await stock.findById(item.id);
-      if (!stock) {
-        return res.status(400);
-      }
-      const so = {
-        userId: savedOrder.id,
-        stockId: item.id,
-        quantity: item.quantity
-      };
+// router.post("/create", async (req, res, next) => {
+//   try {
+//     const savedOrder = await Transaction.create(
+//       req.body,
+//       { w: 1 },
+//       { returning: true }
+//     );
+//     req.body.stocks.forEach(async item => {
+//       const stock = await Stock.findById(item.id);
+//       if (!stock) {
+//         return res.status(400);
+//       }
+//       const so = {
+//         userId: savedOrder.id,
+//         stockId: item.id,
+//         quantity: item.quantity
+//       };
 
-      const savedSO = await Transaction.create(
-        so,
-        { w: 1 },
-        { returning: true }
-      );
+//       const savedSO = await Transaction.create(
+//         so,
+//         { w: 1 },
+//         { returning: true }
+//       );
+//     });
+//     return res.status(200).json(savedOrder);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.post("/purchase", async (req, res, next) => {
+  try {
+    const transaction = await Transaction.create({
+      quantity: req.body.quantity,
+      userId: req.body.userId
     });
-    return res.status(200).json(savedOrder);
+    const stock = await Stock.create({
+      ticker: req.body.ticker,
+      transactionId: transaction.id,
+      userId: transaction.userId,
+      price: 50
+    });
+    transaction.totalPrice = req.body.quantity * stock.price;
+    transaction.save();
+    // console.log(transaction);
+    res.json(transaction);
   } catch (err) {
     next(err);
   }
